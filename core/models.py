c827338c83a6b4b2c04f3f30adc37d73fca9febb
oegpyg <oegpyg@gmail.com>
2023-06-08 23:34:35 -0400

    Se agregan los puntos por encuentro al equipo y color

--- core/models.py
+++ core/models.py
@@ -18,17 +18,24 @@ class Torneo(models.Model):
         return self.nombre
 
 
-class Colores(models.Model):
-    color = models.CharField(max_length=100, unique=True)
+class Color(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    puntos = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.color
+        return self.nombre
+
+    def aumentar_puntos(self, cantidad):
+        self.puntos += cantidad
+        self.save()
 
 
 class Equipo(models.Model):
     nombre = models.CharField(max_length=100)
     torneo = models.ForeignKey(
         Torneo, on_delete=models.CASCADE, related_name='equipos')
+    color = models.ForeignKey(
+        Color, on_delete=models.CASCADE, related_name='colores')
     puntos = models.IntegerField(default=0)
 
     def __str__(self):
@@ -91,8 +98,31 @@ class Encuentro(models.Model):
     resultado_visitante = models.PositiveIntegerField(blank=True, null=True)
     ganador = models.ForeignKey(Equipo, on_delete=models.CASCADE,
                                 blank=True, null=True, related_name='encuentros_ganados')
-    puntos = models.IntegerField(
-        default=0, help_text="Define los puntos otorgados al ganador")
+    puntos_ganador = models.IntegerField(
+        default=3, help_text="Define los puntos otorgados al ganador")
+    puntos_empate = models.IntegerField(
+        default=1, help_text="Define los puntos otorgados al ganador")
+    finalizado = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.equipo_local} vs {self.equipo_visitante}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.finalizado:
+            # Realizar el calculo de puntos cuanto el encuentro finalice
+            if self.resultado_local > self.resultado_visitante:
+                # Sumar puntos al equipo local
+                self.equipo_local.aumentar_puntos(self.puntos_ganador)
+                self.equipo_local.color.aumentar_puntos(self.puntos_ganador)
+            elif self.resultado_local < self.resultado_visitante:
+                # Sumar puntos al equipo visitante
+                self.equipo_visitante.aumentar_puntos(self.puntos_ganador)
+                self.equipo_visitante.color.aumentar_puntos(
+                    self.puntos_ganador)
+            else:
+                # Sumar punto a cada equipo por empate
+                self.equipo_local.aumentar_puntos(self.puntos_empate)
+                self.equipo_local.color.aumentar_puntos(self.puntos_empate)
+                self.equipo_visitante.aumentar_puntos(self.puntos_empate)
+                self.equipo_visitante.color.aumentar_puntos(self.puntos_empate)
